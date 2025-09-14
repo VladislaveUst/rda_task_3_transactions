6559035c0b3f01fcd55249284b102388e6741f57
Ustiantsev Vladislav <vladisloveust@gmail.com>
2025-09-15 00:51:41 +0300

    Solution2

--- task.sql
+++ task.sql
@@ -1,24 +1,21 @@
 -- Use our database
 USE ShopDB;
 
+-- Some data should be created outside the transaction (here)
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, '2023-01-01');
+
 -- Start the transaction
 START TRANSACTION;
 
--- Step 1: Create a new order for CustomerID = 1
-INSERT INTO Orders (CustomerID, Date)
-VALUES (1, CURRENT_DATE());
-
--- Step 2: Get the last inserted Order ID
+-- And some data should be created inside the transaction
 SET @order_id = LAST_INSERT_ID();
 
--- Step 3: Insert the order item (ProductID = 1, Count = 1)
 INSERT INTO OrderItems (OrderID, ProductID, Count)
 VALUES (@order_id, 1, 1);
 
--- Step 4: Update product stock (reduce by 1)
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - 1
 WHERE ID = 1;
 
--- Step 5: Commit the transaction
 COMMIT;

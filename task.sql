a32cc6963d2af9a996d6dd5c5947ba679f752456
Ustiantsev Vladislav <vladisloveust@gmail.com>
2025-09-15 19:35:38 +0300

    Solution4

--- task.sql
+++ task.sql
@@ -1,28 +1,28 @@
--- Use our database
-DROP DATABASE IF EXISTS ShopDB
-CREATE DATABASE ShopDB
+
+DROP DATABASE IF EXISTS ShopDB;
+CREATE DATABASE ShopDB;
 
 USE ShopDB;
 
--- Some data should be created outside the transaction (here)
+SOURCE create-database.sql;
 
 
--- Start the transaction
 START TRANSACTION;
 
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
 
 SET @order_id = LAST_INSERT_ID();
-
 SET @product_id = 1;
 SET @count = 1;
 
-INSERT INTO OrderItems (OrderID, ProductID, Count)
+INSERT INTO OrderItems (OrderID, ProductID, `Count`)
 VALUES (@order_id, @product_id, @count);
 
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - @count
-WHERE ID = @product_id;
+WHERE ID = @product_id
+  AND WarehouseAmount >= @count;
 
 COMMIT;
+
